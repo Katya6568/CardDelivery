152091933deab09a66dde92bb8d0db5017c46762
Ekaterina Pavlova <sone4ka1414@gmail.com>
2023-06-15 15:35:17 +0300

    tests

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -1,14 +1,15 @@
 package ru.netology.delivery.data;
 
 import com.github.javafaker.Faker;
+import lombok.Data;
 import lombok.Value;
-import lombok.val;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Random;
 
+@Data
 public class DataGenerator {
     private DataGenerator() {
     }
@@ -16,25 +17,49 @@ public class DataGenerator {
     public static String generateDate(int shift) {
         // TODO: добавить логику для объявления переменной date и задания её значения, для генерации строки с датой
         // Вы можете использовать класс LocalDate и его методы для получения и форматирования даты
-        return date;
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
     }
 
-    public static String generateCity(String locale) {
+    public static String generateCity() {
         // TODO: добавить логику для объявления переменной city и задания её значения, генерацию можно выполнить
         // с помощью Faker, либо используя массив валидных городов и класс Random
+        String[] cities = {
+                "Архангельск",
+                "Астрахань",
+                "Благовещенск",
+                "Владивосток",
+                "Горно-Алтайск",
+                "Краснодар",
+                "Красноярск",
+                "Магас",
+                "Мурманск",
+                "Саранск",
+                "Петропавловск-Камчатский",
+                "Хабаровск",
+                "Ханты-Мансийск",
+                "Южно-Сахалинск",
+                "Москва",
+                "Санкт-Петербург"
+        };
+
+        Random random = new Random();
+        String city = cities[random.nextInt(cities.length)];
         return city;
     }
 
     public static String generateName(String locale) {
         // TODO: добавить логику для объявления переменной name и задания её значения, для генерации можно
         // использовать Faker
-        return name;
+        Faker faker = new Faker(new Locale(locale));
+        return faker.name().fullName();
     }
 
     public static String generatePhone(String locale) {
         // TODO: добавить логику для объявления переменной phone и задания её значения, для генерации можно
         // использовать Faker
-        return phone;
+        Faker faker = new Faker(new Locale(locale));
+        return faker.phoneNumber().phoneNumber();
     }
 
     public static class Registration {
@@ -44,7 +69,7 @@ public class DataGenerator {
         public static UserInfo generateUser(String locale) {
             // TODO: добавить логику для создания пользователя user с использованием методов generateCity(locale),
             // generateName(locale), generatePhone(locale)
-            return user;
+            return new UserInfo(generateCity(), generateName(locale), generatePhone(locale));
         }
     }
 
--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -1,11 +1,23 @@
 package ru.netology.delivery.test;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
+import net.bytebuddy.asm.Advice;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 import ru.netology.delivery.data.DataGenerator;
 
-import static com.codeborne.selenide.Selenide.open;
+import javax.xml.crypto.Data;
+import java.time.Duration;
+import java.util.Locale;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
 
 class DeliveryTest {
 
@@ -27,5 +39,170 @@ class DeliveryTest {
         // firstMeetingDate и secondMeetingDate. Можно также вызывать методы generateCity(locale),
         // generateName(locale), generatePhone(locale) для генерации и получения в тесте соответственно города,
         // имени и номера телефона без создания пользователя в методе generateUser(String locale) в датагенераторе
+        open("http://localhost:9999/");
+        $("[data-test-id= 'city'] input").setValue(validUser.getCity());
+        $("[data-test-id= 'date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id= 'date'] input").setValue(firstMeetingDate);
+        $("[data-test-id= 'name'] input").setValue(validUser.getName());
+        $("[data-test-id= 'phone'] input").setValue(validUser.getPhone());
+        $("[data-test-id= 'agreement']").click();
+        $$("[type= 'button']").find(Condition.text("Запланировать")).click();
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + firstMeetingDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
+        $$("[type= 'button']").filter(Condition.visible).first().click();
+        $("[data-test-id= 'date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id= 'date'] input").setValue(secondMeetingDate);
+        $$("[type= 'button']").find(Condition.text("Запланировать")).click();
+        $("[data-test-id= 'replan-notification']")
+                .shouldBe(Condition.visible)
+                .shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"), Duration.ofSeconds(20));
+        $$("[type= 'button']").filter(Condition.visible).first().click();
+        //$("[data-test-id= 'success-notification']").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + secondMeetingDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
+
+    @Test
+    @DisplayName("Should not accept phone of 10 numbers")
+    void shouldNotAcceptPhone10Numbers() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        open("http://localhost:9999/");
+        $("[data-test-id= 'city'] input").setValue(validUser.getCity());
+        $("[data-test-id= 'date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id= 'date'] input").setValue(DataGenerator.generateDate(5));
+        $("[data-test-id= 'name'] input").setValue(validUser.getName());
+        $("[data-test-id= 'phone'] input").setValue("+7951256789");
+        $("[data-test-id= 'agreement']").click();
+        $$("[type= 'button']").find(Condition.text("Запланировать")).click();
+        $("[data-test-id= 'phone'].input_invalid .input__sub")
+                .shouldBe(Condition.visible)
+                .shouldHave(Condition.text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    @DisplayName("Should not accept phone of 1 number")
+    void shouldNotAcceptPhoneOf1Number() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        open("http://localhost:9999/");
+        $("[data-test-id= 'city'] input").setValue(validUser.getCity());
+        $("[data-test-id= 'date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id= 'date'] input").setValue(DataGenerator.generateDate(5));
+        $("[data-test-id= 'name'] input").setValue(validUser.getName());
+        $("[data-test-id= 'phone'] input").setValue("+7");
+        $("[data-test-id= 'agreement']").click();
+        $$("[type= 'button']").find(Condition.text("Запланировать")).click();
+        $("[data-test-id= 'phone'].input_invalid .input__sub")
+                .shouldBe(Condition.visible)
+                .shouldHave(Condition.text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    @DisplayName("Should not accept phone of plus")
+    void shouldNotAcceptPhoneOfPlus() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        open("http://localhost:9999/");
+        $("[data-test-id= 'city'] input").setValue(validUser.getCity());
+        $("[data-test-id= 'date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id= 'date'] input").setValue(DataGenerator.generateDate(5));
+        $("[data-test-id= 'name'] input").setValue(validUser.getName());
+        $("[data-test-id= 'phone'] input").setValue("+");
+        $("[data-test-id= 'agreement']").click();
+        $$("[type= 'button']").find(Condition.text("Запланировать")).click();
+        $("[data-test-id= 'phone'].input_invalid .input__sub")
+                .shouldBe(Condition.visible)
+                .shouldHave(Condition.text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    @DisplayName("Should not replan if phone changed")
+    void shouldNotReplanIfPhoneChanged() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 5;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+
+        open("http://localhost:9999/");
+        $("[data-test-id= 'city'] input").setValue(validUser.getCity());
+        $("[data-test-id= 'date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id= 'date'] input").setValue(firstMeetingDate);
+        $("[data-test-id= 'name'] input").setValue(validUser.getName());
+        $("[data-test-id= 'phone'] input").setValue(validUser.getPhone());
+        $("[data-test-id= 'agreement']").click();
+        $$("[type= 'button']").find(Condition.text("Запланировать")).click();
+        $(".notification__content")
+                .shouldBe(Condition.visible)
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + firstMeetingDate), Duration.ofSeconds(15));
+        $$("[type= 'button']").filter(Condition.visible).first().click();
+        $("[data-test-id= 'phone'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id= 'phone'] input").setValue(validUser.getPhone());
+        $$("[type= 'button']").find(Condition.text("Запланировать")).click();
+        $("[data-test-id= 'replan-notification']")
+                .shouldBe(Condition.hidden)
+                .shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"), Duration.ofSeconds(20));
+
+    }
+
+    @Test
+    @DisplayName("Should not replan if same date")
+    void shouldNotReplanIfSameDate() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+
+        open("http://localhost:9999/");
+        $("[data-test-id= 'city'] input").setValue(validUser.getCity());
+        $("[data-test-id= 'date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id= 'date'] input").setValue(firstMeetingDate);
+        $("[data-test-id= 'name'] input").setValue(validUser.getName());
+        $("[data-test-id= 'phone'] input").setValue(validUser.getPhone());
+        $("[data-test-id= 'agreement']").click();
+        $$("[type= 'button']").find(Condition.text("Запланировать")).click();
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + firstMeetingDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
+        $$("[type= 'button']").filter(Condition.visible).first().click();
+        $("[data-test-id= 'date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id= 'date'] input").setValue(firstMeetingDate);
+        $$("[type= 'button']").find(Condition.text("Запланировать")).click();
+        $("[data-test-id= 'replan-notification']")
+                .shouldBe(Condition.hidden)
+                .shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"), Duration.ofSeconds(20));
+
+    }
+
+    @Test
+    @DisplayName("Should not accept name of hyphens")
+    void shouldNotAcceptNameOfHyphens() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        open("http://localhost:9999/");
+        $("[data-test-id= 'city'] input").setValue(validUser.getCity());
+        $("[data-test-id= 'date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id= 'date'] input").setValue(DataGenerator.generateDate(5));
+        $("[data-test-id= 'name'] input").setValue("------");
+        $("[data-test-id= 'phone'] input").setValue(validUser.getPhone());
+        $("[data-test-id= 'agreement']").click();
+        $$("[type= 'button']").find(Condition.text("Запланировать")).click();
+        $("[data-test-id= 'name'].input_invalid .input__sub")
+                .shouldBe(Condition.visible)
+                .shouldHave(Condition.text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    @DisplayName("Should accept name of Cyrillic")
+    void shouldAcceptNameOfCyrillic() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        open("http://localhost:9999/");
+        $("[data-test-id= 'city'] input").setValue(validUser.getCity());
+        $("[data-test-id= 'date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id= 'date'] input").setValue(DataGenerator.generateDate(5));
+        $("[data-test-id= 'name'] input").setValue("Пётр Иванов");
+        $("[data-test-id= 'phone'] input").setValue("+79512567891");
+        $("[data-test-id= 'agreement']").click();
+        $$("[type= 'button']").find(Condition.text("Запланировать")).click();
+        $(".notification__content")
+                .shouldBe(Condition.visible)
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + DataGenerator.generateDate(5)), Duration.ofSeconds(15));
+    }
+
 }
