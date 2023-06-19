35b699851bdbcf9d23d4cac0e468cbe3218c60fb
Katya6568 <125563119+Katya6568@users.noreply.github.com>
2023-06-19 08:45:26 +0300

    Update DataGenerator.java

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -15,15 +15,11 @@ public class DataGenerator {
     }
 
     public static String generateDate(int shift) {
-        // TODO: добавить логику для объявления переменной date и задания её значения, для генерации строки с датой
-        // Вы можете использовать класс LocalDate и его методы для получения и форматирования даты
         return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
     }
 
     public static String generateCity() {
-        // TODO: добавить логику для объявления переменной city и задания её значения, генерацию можно выполнить
-        // с помощью Faker, либо используя массив валидных городов и класс Random
         String[] cities = {
                 "Архангельск",
                 "Астрахань",
@@ -49,15 +45,11 @@ public class DataGenerator {
     }
 
     public static String generateName(String locale) {
-        // TODO: добавить логику для объявления переменной name и задания её значения, для генерации можно
-        // использовать Faker
         Faker faker = new Faker(new Locale(locale));
         return faker.name().fullName();
     }
 
     public static String generatePhone(String locale) {
-        // TODO: добавить логику для объявления переменной phone и задания её значения, для генерации можно
-        // использовать Faker
         Faker faker = new Faker(new Locale(locale));
         return faker.phoneNumber().phoneNumber();
     }
@@ -66,11 +58,9 @@ public class DataGenerator {
         private Registration() {
         }
 
-        public static UserInfo generateUser(String locale) {
-            // TODO: добавить логику для создания пользователя user с использованием методов generateCity(locale),
-            // generateName(locale), generatePhone(locale)
-            return new UserInfo(generateCity(), generateName(locale), generatePhone(locale));
-        }
+     public static UserInfo generateUser(String locale) {
+        return new UserInfo(generateCity(), generateName(locale), generatePhone(locale));
+     }
     }
 
     @Value
